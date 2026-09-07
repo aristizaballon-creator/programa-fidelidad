4d0b859afab86680d5dae3ca375be691af49dfd0
aristizaballon-creator <aristizabal.lon@gmail.com>
2026-09-06 19:51:49 -0500

    Corregir application.properties de pruebas y CORS dinámico de Codespaces

--- scripts/levantar.sh
+++ scripts/levantar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$CODESPACES" = "true" ]; then
   export VITE_API_URL=https://${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}
+  export FRONTEND_ORIGIN=https://${CODESPACE_NAME}-5173.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}
   echo "Detecté Codespaces, usando VITE_API_URL=$VITE_API_URL"
 fi
 docker compose up --build -d
